70fd15d15f4c6c53a423a7bce214cf9f2d427903
MaySantucci <may.santucci@gmail.com>
2018-04-04 09:58:40 +0200

    Connection works but JSON don't works

--- app/src/main/java/com/example/santu/nearme/EventsFragment.java
+++ app/src/main/java/com/example/santu/nearme/EventsFragment.java
@@ -1,17 +1,60 @@
 package com.example.santu.nearme;
 
 import android.content.Context;
-import android.net.Uri;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.app.ProgressDialog;
 
-public class EventsFragment extends ListFragment implements AdapterView.OnItemClickListener {
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+public class EventsFragment extends ListFragment {
+
+
+    private ProgressDialog pDialog;
+    // Creating JSON Parser object
+    JSONParser jParser = new JSONParser();
+
+    ArrayList<HashMap<String, String>> eventsList;
+
+
+    // url to get all products list
+    private static String url_all_events = "http://192.168.43.67/api.toponconcert.info/get_all_events.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_EVENTS = "events";
+    private static final String TAG_lOCALE = "locale";
+    private static final String TAG_ARTISTA = "artista";
+    private static final String TAG_DATA = "data";
+    private static final String TAG_ORA = "ora";
+    private static final String TAG_DESCRIZIONE = "descrizione";
+
+    // events JSONArray
+    JSONArray events = null;
+
     public EventsFragment() {
         // Required empty public constructor
     }
@@ -22,17 +65,107 @@ public class EventsFragment extends ListFragment implements AdapterView.OnItemCl
         return rootView;
     }
 
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.Planets, android.R.layout.simple_list_item_1);
-        setListAdapter(adapter);
-        getListView().setOnItemClickListener(this);
-    }
-     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+        eventsList = new ArrayList<HashMap<String, String>>();
+
+        // Loading products in Background Thread
+        new LoadAllEvents().execute();
     }
 
+
+    /**
+     * Background Async Task to Load all product by making HTTP Request
+     */
+    class LoadAllEvents extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        /**
+         * getting All products from url
+         */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            // getting JSON string from URL
+            JSONObject json = jParser.makeHttpRequest(url_all_events, "GET", params);
+
+
+            // Check your log cat for JSON reponse
+            Log.d("All Products: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // products found
+                    // Getting Array of Products
+                    events = json.getJSONArray(TAG_EVENTS);
+
+                    // looping through All Products
+                    for (int i = 0; i < events.length(); i++) {
+                        JSONObject c = events.getJSONObject(i);
+
+                        // Storing each json item in variable
+                        String locale = c.getString(TAG_lOCALE);
+                        String artista = c.getString(TAG_ARTISTA);
+
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        map.put(TAG_lOCALE, locale);
+                        map.put(TAG_ARTISTA, artista);
+
+                        // adding HashList to ArrayList
+                        eventsList.add(map);
+                    }
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+
+            // updating UI from Background Thread
+            try {
+                getActivity().runOnUiThread(new Runnable() {
+                    public void run() {
+                        /**
+                         * Updating parsed JSON data into ListView
+                         * */
+                        ListAdapter adapter = new SimpleAdapter(
+                                getActivity(), eventsList,
+                                R.layout.event_row, new String[]{TAG_lOCALE,
+                                TAG_ARTISTA},
+                                new int[]{R.id.locale, R.id.artista});
+                        // updating listview
+                        setListAdapter(adapter);
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
--- app/src/main/java/com/example/santu/nearme/MainActivity.java
+++ app/src/main/java/com/example/santu/nearme/MainActivity.java
@@ -1,29 +1,19 @@
 package com.example.santu.nearme;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.view.LayoutInflater;
-
 public class MainActivity extends DrawerMenuActivity {
 
-    private Fragment results;
     private TabLayout tabLayout;
     private ViewPager viewPager;
-    int position;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewPager = findViewById(R.id.viewpager);
+
 
         // Create an adapter that knows which fragment should be shown on each page
         PagerAdapter adapter = new PagerAdapter(this, getSupportFragmentManager());
